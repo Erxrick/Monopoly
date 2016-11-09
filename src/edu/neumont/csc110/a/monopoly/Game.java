749f0bdca36dac496af945edfa963ffe66303a60
Zachery Anderson <zanderson@student.neumont.edu>
2016-11-09 12:25:50 -0700

    This is the first edit to the repository. In this I have created the majority of the initial classes that shall be need in the project.

---
+++ src/edu/neumont/csc110/a/monopoly/Game.java
@@ -0,0 +1,8 @@
+package edu.neumont.csc110.a.monopoly;
+
+public class Game {
+
+	public void run() {
+		//this is what actually plays the game
+	}
+}
---
+++ src/edu/neumont/csc110/a/monopoly/Player.java
@@ -0,0 +1,5 @@
+package edu.neumont.csc110.a.monopoly;
+
+public class Player {
+
+}
