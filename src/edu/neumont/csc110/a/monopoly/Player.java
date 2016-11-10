7278ce4fbcf1da91c7b827dd860740419f7b986d
Zachery Anderson <zanderson@student.neumont.edu>
2016-11-10 14:32:42 -0700

    Added the cards

--- src/edu/neumont/csc110/a/monopoly/Player.java
+++ src/edu/neumont/csc110/a/monopoly/Player.java
@@ -2,9 +2,27 @@ package edu.neumont.csc110.a.monopoly;
 
 public class Player {
 	private String name;
-	private double money;
+	private int money;
+	private int getOutOfJailCard;
+	private boolean isPlayerInJail;
 	
 	
+	public boolean isPlayerInJail() {
+		return isPlayerInJail;
+	}
+
+	public void setPlayerInJail(boolean isPlayerInJail) {
+		this.isPlayerInJail = isPlayerInJail;
+	}
+
+	public int getGetOutOfJailCard() {
+		return getOutOfJailCard;
+	}
+
+	public void setGetOutOfJailCard(int getOutOfJailCard) {
+		this.getOutOfJailCard = getOutOfJailCard;
+	}
+
 	public void setStartingMoney() {
 		money = 1500;
 	}
@@ -17,11 +35,11 @@ public class Player {
 		this.name = name;
 	}
 
-	public double getMoney() {
+	public int getMoney() {
 		return money;
 	}
 
-	public void setMoney(double money) {
-		this.money = money;
+	public void addMoney(int money) {
+		this.money += money;
 	}
 }
