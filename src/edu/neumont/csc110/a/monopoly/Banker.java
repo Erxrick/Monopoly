0b1fc279fdf4dc61a68d16c1e2054dedb80f9552
Zachery Anderson <zanderson@student.neumont.edu>
2016-11-17 11:10:12 -0700

    Fixed buying from the banker

--- src/edu/neumont/csc110/a/monopoly/Banker.java
+++ src/edu/neumont/csc110/a/monopoly/Banker.java
@@ -2,40 +2,39 @@ package edu.neumont.csc110.a.monopoly;
 import java.util.ArrayList;
 
 public class Banker {
-	BoardTiles bTiles = new BoardTiles();
+	//BoardTiles bTiles = new BoardTiles();
 	private ArrayList<PropertyCards> property;
 	
-	public void bankinit() {
+	public void bankinit(BoardTiles allTheProperty) {
 		property = new ArrayList<PropertyCards>();
-		bTiles.init();
-		property.add(bTiles.Mediterranean_Avenue);
-		property.add(bTiles.Baltic_Avenue);
-		property.add(bTiles.Oriental_Avenue);
-		property.add(bTiles.Vermont_Avenue);
-		property.add(bTiles.Connecticut_Avenue);
-		property.add(bTiles.StCharles_Place);
-		property.add(bTiles.States_Avenue);
-		property.add(bTiles.Virginia_Avenue);
-		property.add(bTiles.StJames_Place);
-		property.add(bTiles.Tennessee_Avenue);
-		property.add(bTiles.New_York_Avenue);
-		property.add(bTiles.Kentucky_Avenue);
-		property.add(bTiles.Indiana_Avenue);
-		property.add(bTiles.Illinois_Avenue);
-		property.add(bTiles.Atlantic_Avenue);
-		property.add(bTiles.Ventor_Avenue);
-		property.add(bTiles.Marvin_Gardens);
-		property.add(bTiles.Pacific_Avenue);
-		property.add(bTiles.North_Carolina_Avenue);
-		property.add(bTiles.Pennslvania_Avenue);
-		property.add(bTiles.Park_Place);
-		property.add(bTiles.Boardwalk);
-		property.add(bTiles.Reading_Railroad);
-		property.add(bTiles.Pennsylvania_Railroad);
-		property.add(bTiles.BO_Railroad);
-		property.add(bTiles.Short_Line);
-		property.add(bTiles.Electric_Company);
-		property.add(bTiles.Water_Works);
+		property.add(allTheProperty.Mediterranean_Avenue);
+		property.add(allTheProperty.Baltic_Avenue);
+		property.add(allTheProperty.Oriental_Avenue);
+		property.add(allTheProperty.Vermont_Avenue);
+		property.add(allTheProperty.Connecticut_Avenue);
+		property.add(allTheProperty.StCharles_Place);
+		property.add(allTheProperty.States_Avenue);
+		property.add(allTheProperty.Virginia_Avenue);
+		property.add(allTheProperty.StJames_Place);
+		property.add(allTheProperty.Tennessee_Avenue);
+		property.add(allTheProperty.New_York_Avenue);
+		property.add(allTheProperty.Kentucky_Avenue);
+		property.add(allTheProperty.Indiana_Avenue);
+		property.add(allTheProperty.Illinois_Avenue);
+		property.add(allTheProperty.Atlantic_Avenue);
+		property.add(allTheProperty.Ventor_Avenue);
+		property.add(allTheProperty.Marvin_Gardens);
+		property.add(allTheProperty.Pacific_Avenue);
+		property.add(allTheProperty.North_Carolina_Avenue);
+		property.add(allTheProperty.Pennslvania_Avenue);
+		property.add(allTheProperty.Park_Place);
+		property.add(allTheProperty.Boardwalk);
+		property.add(allTheProperty.Reading_Railroad);
+		property.add(allTheProperty.Pennsylvania_Railroad);
+		property.add(allTheProperty.BO_Railroad);
+		property.add(allTheProperty.Short_Line);
+		property.add(allTheProperty.Electric_Company);
+		property.add(allTheProperty.Water_Works);
 	}
 	public void removeCard(PropertyCards card) {
 		int index = this.property.indexOf(card);
