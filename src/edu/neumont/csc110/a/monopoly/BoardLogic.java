2a929c4bcd0f1bf157303ef261ee19a67ca0a451
Gabe Barber <GBarber@student.neumont.edu>
2016-11-17 08:39:05 -0700

    stuff

--- src/edu/neumont/csc110/a/monopoly/BoardLogic.java
+++ src/edu/neumont/csc110/a/monopoly/BoardLogic.java
@@ -2,89 +2,43 @@ package edu.neumont.csc110.a.monopoly;
 
 public class BoardLogic {
 
+	public static boolean canbeBought();
 
-public static void Boardwalk(PropertyCards cards) {
-	if(cards.isBought() == true){
-		
-	}
-	
-}
 
-public static void Luxury_Tax() {
-	// TODO Auto-generated method stub
+
+public static void Luxury_Tax(Player player) {
+	player.addMoney(-75);
 	
 }
 
-public static void Park_Place() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Short_Line() {
-	// TODO Auto-generated method stub
-	
-}
-public static void Pennslvania_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
-static void North_Carolina_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Pacific_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
+
+
+
+
+
 
 public static void Go_To_Jail() {
 	// TODO Auto-generated method stub
 	
 }
 
-public static void Marvin_Gardens() {
-	// TODO Auto-generated method stub
-	
-}
-
-public static void Water_Works() {
-	// TODO Auto-generated method stub
-	
-}
-
-public static void Ventor_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
-
-public static void Atlantic_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
 
 
-public static void BO_Railroad() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Illinois_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Indiana_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Kentucky_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
+
+
+
+
+
+
+
+
 
 public static void Free_Parking() {
 	// TODO Auto-generated method stub
@@ -93,98 +47,57 @@ public static void Free_Parking() {
 
 
 
-public static void New_York_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
-
-public static void Tennessee_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
 
 
-public static void StJames_Place() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Pennsylvania_Railroad() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Virginia_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void States_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Electric_Company() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void StCharles_Place() {
-	// TODO Auto-generated method stub
-	
-}
+
+
+
+
+
+
+
+
+
 
 public static void Jail() {
-	// TODO Auto-generated method stub
+	
 	
 }
 
-public static void Connecticut_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
 
-public static void Vermont_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
+
+
 
 public static void Chance() {
-	// TODO Auto-generated method stub
 	
-}
-
-public static void Oriental_Avenue() {
-	// TODO Auto-generated method stub
 	
 }
 
 
 
-public static void Reading_Railroad() {
-	// TODO Auto-generated method stub
-	
-}
+
+
+
 public static void Income_Tax() {
 	// TODO Auto-generated method stub
 	
 }
 
-public static void Baltic_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
+
 
 public static void Community_Chest() {
 	// TODO Auto-generated method stub
 	
 }
 
-public static void Mediterranean_Avenue() {
-	// TODO Auto-generated method stub
-	
-}
+
+
 
 public static void Go() {
 	// TODO Auto-generated method stub
